4b4da1ce9f7a7f1f66df9e9021f3dbbd57f0b740
zakrroma <zakrroma@gmail.com>
2026-09-14 20:29:49 +0300

    push

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,6 +14,8 @@ public class AppManager {
 
     @BeforeMethod(alwaysRun = true)
     public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
